b8a68ab545a2956cc2b38d1fc4877d6024505d7e
shimaenaga <admin@shimaenaga.dev>
2025-11-21 19:50:48 +0900

    bug fix, test can be false

--- server/src/index.js
+++ server/src/index.js
@@ -1,22 +1,24 @@
 import { buildSolvedMessage } from "./module/dmessage";
 
+const headers = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'POST',
+  'Access-Control-Allow-Headers': 'Content-Type',
+}
+
 export default {
   async fetch(req, env){
     if (req.method !== 'POST') {
       if (req.method === "OPTIONS") {
         return new Response(null, {
           status: 204,
-          headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'POST',
-            'Access-Control-Allow-Headers': 'Content-Type',
-          },
+          headers
         });
       }
-      return new Response('Method Not Allowed', { status: 405, headers: { Allow: 'POST' } });
+      return new Response('Method Not Allowed', { status: 405, headers});
     }
     if (!req.headers.get('Content-Type')?.startsWith('application/json')) {
-      return new Response('Invalid Content-Type', { status: 400 });
+      return new Response('Invalid Content-Type', { status: 400, headers});
     }
     let requestBody
     try {
@@ -25,13 +27,15 @@ export default {
       console.error("Not a valid JSON");
       return new Response('The request body is not valid', {
         status: 400,
+        headers
       })
     }
 
-    if (!requestBody.challengeId || !requestBody.test || !requestBody.solver) {
+    if (!requestBody.challengeId || requestBody.test === undefined || !requestBody.solver) {
       console.error(JSON.stringify(requestBody));
       return new Response('Invalid request body', {
         status: 400,
+        headers
       })
     }
 
@@ -53,15 +57,18 @@ export default {
         console.error(await response.text());
         return new Response('Discord webhook failed', {
           status: 500,
+          headers
         })
       }
       return new Response("ok", {
         status: 200,
+        headers
       })
     } catch(error){
       console.error(error);
       return new Response('Internal Server Error', {
         status: 500,
+        headers
       })
     }
   }
